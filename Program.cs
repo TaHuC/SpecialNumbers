4f4422188c54226efba54c83cc46e91732cda3af
TaHuC <TaHuC@hotmail.com>
2017-01-24 22:27:14 +0200

    Finish Special Numebers

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace SpecialNumbers
 	{
 		public static void Main(string[] args)
 		{
-			
+			int inputNum = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= inputNum; i++)
+			{
+				int sumOfDigits = 0;
+				int digits = i;
+				while (digits > 0)
+				{
+					sumOfDigits += digits % 10;
+					digits = digits / 10;
+				}
+				bool special = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+				Console.WriteLine("{0} -> {1}", i, special);
+			}
 		}
 	}
 }
